b708553823ff14585844c77948b0ac90915dbac6
Nguyen Quang Huy <60919679+gosu12@users.noreply.github.com>
2020-09-28 17:57:13 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,4 @@
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
-for (let item of anchorlinks) {
-    item.addEventListener('click', (e)=> {
-        let hashval = item.getAttribute('href')
-        let target = document.querySelector(hashval)
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-        history.pushState(null, null, hashval)
-        e.preventDefault()
-    })
-}
+
 function myFunction4() {
     let str = "my github";
     document.getElementById("link").innerHTML = str.link("http://github.com/gosu12");
